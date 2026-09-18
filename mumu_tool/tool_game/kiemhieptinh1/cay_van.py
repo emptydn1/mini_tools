c38f4ba098649e48fc8f04c711ae02e184af904d
Your Name <your.email@example.com>
2026-09-18 19:02:55 +0700

    o

--- mumu_tool/tool_game/kiemhieptinh1/cay_van.py
+++ mumu_tool/tool_game/kiemhieptinh1/cay_van.py
@@ -4,64 +4,7 @@ import threading
 
 from mumu_tool.config import merge_devices
 from mumu_tool.adb_core import check_shells_created, tap_points, tap_all_devices, input_text, tap, swipe, input_text_list
-
-
-class QuanLyTaiKhoan:
-    def __init__(self):
-        self.da_nhap = False
-        self.so_dau = 0
-        self.ten = ""
-        self.so_bat_dau = 0
-        self.so_ket_thuc = 0
-        self.gia_tri_tang_giam = 3
-
-    def nhap_du_lieu(self):
-        value = input("Nhập dữ liệu (ví dụ 1-huy-1+16): ").strip()
-
-        arr = value.split("-")
-
-        if len(arr) != 3:
-            print("Sai định dạng!")
-            return False
-
-        try:
-            self.so_dau = int(arr[0])
-            self.ten = arr[1]
-            self.so_bat_dau, self.so_ket_thuc = map(int, arr[2].split("+"))
-
-        except ValueError:
-            print("Sai định dạng!")
-            return False
-
-        self.da_nhap = True
-        return True
-
-    def nhap_du_lieu_gia_tri_tang_giam(self):
-        value = input("Nhập giá trị tăng giảm: ").strip()
-
-        if value.isdigit():
-            self.gia_tri_tang_giam = int(value)
-        else:
-            print("Sai định dạng, vui lòng nhập số")
-
-    def arr_tai_khoan(self):
-        return [f"{self.so_dau}{self.ten}{so_thu_tu}" for so_thu_tu in range(self.so_bat_dau, self.so_ket_thuc + 1)]
-
-    def tang_so_dau(self):
-        if not self.da_nhap:
-            print("Bạn cần nhập dữ liệu trước")
-            return
-
-        self.so_dau += self.gia_tri_tang_giam
-        input_text_list(self.arr_tai_khoan())
-
-    def giam_so_dau(self):
-        if not self.da_nhap:
-            print("Bạn cần nhập dữ liệu trước")
-            return
-
-        self.so_dau -= self.gia_tri_tang_giam
-        input_text_list(self.arr_tai_khoan())
+from mumu_tool.tool_game.utils import QuanLyTaiKhoan
 
 
 def cay_van():
